b9fee09b54514cf82a1143b044e93e440ded753f
renciweb <mwatson@renci.org>
2018-12-05 10:38:14 -0500

    add tool to scrape md files for DOIs and build JSON library

---
+++ utils/scrape.py
@@ -0,0 +1,43 @@
+import re
+
+def scraper(filename):
+    '''
+    Scrape a single file for lines that look like '- DOI: 10.1021/jp046536e'
+    '''
+    dois = []
+    doi_regex = '- DOI: (.+\/.+)$'
+    with open(filename) as f:
+        dois = []
+        doi_lines = [line.strip() for line in f.readlines() if '- DOI:' in line]
+        for line in doi_lines:
+            doi = re.match(doi_regex, line)
+            if doi:
+                dois.append(doi.groups()[0])
+        return dois
+
+def scrape_files_for_dois(filenames):
+    '''
+    Call DOI scraper on a list of filenames
+    '''
+    dois = []
+    for filename in filenames:
+        print(f'Scraping "{ filename }"...')
+        dois += scraper(filename)
+    dois = list(set(dois))
+    print(f'> Scraped { len(dois) } DOIs.')
+    return dois
+
+def write_dois(filename, dois):
+    with open(filename, 'w') as f:
+        for doi in dois:
+            f.write(doi + '\n')
+    print(f'> Wrote { len(dois) } DOIs to { filename }.')
+
+###
+
+# Filenames are 2005.ms, 2006.md, ..., 2018.md
+doi_files = [f'../library/{i}.md' for i in range(2005, 2019)]
+# Scrape each filename for DOIs
+dois = scrape_files_for_dois(doi_files)
+# Write DOI list to output file
+write_dois('dois.txt', dois)
